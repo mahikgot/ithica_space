d0f2ce58b93ff736443378b6062234d4b1962085
Mark Guiang <mahikgot@gmail.com>
2022-03-29 07:20:41 +0800

    remove self parameter on output argument

--- app.py
+++ app.py
@@ -215,4 +215,4 @@ def main(text):
 gradio.Interface(
         main,
         "text",
-        ["html", gradio.outputs.Label(self, label="Geographical Attribution")]).launch(enable_queue=True)
+        ["html", gradio.outputs.Label(label="Geographical Attribution")]).launch(enable_queue=True)
